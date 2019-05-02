cef9aed2d8c2a59b21aa3dc87e47569d0a16e921
YoungTakhin <850189787@qq.com>
2019-05-02 14:12:02 +0800

    数据库模型

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -5,23 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 //use App\Http\Controllers\Storage;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class TeacherController extends Controller {
 	
+    //对象转数组
+    private function objectToArray($object) {
+        return json_decode(json_encode($object), true);
+    }
+
     //教师端课程查询
     public function selectCourse() {
         session_start();
-        $conn = mysqli_connect("localhost", "root", "ydx970516", "kj");
-        mysqli_select_db($conn, "kj") or die("数据库访问错误" . mysql_error());
-        mysqli_query($conn, "set names UTF8");
-        $result = mysqli_query($conn, "select * from teacher_course join teachers on teachers.id = teacher_course.teacherid join courses on courses.courseid = teacher_course.courseid where teacherid like '" . $_SESSION['id'] . "'");
-        $row_num = mysqli_num_rows($result);
-        $row[0] = NULL;
-        for($i = 0; $i < $row_num; $i++) {
-            $row[$i] = mysqli_fetch_assoc($result);
-        }
-        $course = array('row_num' => $row_num, 'row' => $row);
-        mysqli_close($conn);
+        $id = $_SESSION['id'];
+        $result = DB::table('teacher_course')
+            ->join('teachers', function ($join) {
+                $join->on('teachers.id', '=', 'teacher_course.teacherid');
+            })
+            ->join('courses', function ($join) use ($id) {
+                $join->on('courses.courseid', '=', 'teacher_course.courseid')
+                    ->where('teacherid', 'LIKE', $id);
+            })
+            ->get();
+        $row_num = $result->count();
+        $result = $this::objectToArray($result);
+        $course = array('row_num' => $row_num, 'row' => $result);
         return view('/teacher/selectCourse')->with('course', $course);
     }
 
@@ -34,15 +42,18 @@ class TeacherController extends Controller {
             session_start();
             $id = $_SESSION['id'];
         }
-        $conn = mysqli_connect("localhost", "root", "ydx970516", "kj");
-        mysqli_select_db($conn, "kj") or die("数据库访问错误" . mysql_error());
-        mysqli_query($conn, "set names UTF8");
-        $result = mysqli_query($conn, "select * from teacher_course join teachers on teachers.id = teacher_course.teacherid join courses on courses.courseid = teacher_course.courseid where teacherid like '" . $_SESSION['id'] . "'");
-        $row_num = mysqli_num_rows($result);
-        for($i = 0; $i < $row_num; $i++) { 
-            $row[$i] = mysqli_fetch_assoc($result);
-        }
-        $course = array('row_num' => $row_num, 'row' => $row);
+        $result = DB::table('teacher_course')
+            ->join('teachers', function ($join) {
+                $join->on('teachers.id', '=', 'teacher_course.teacherid');
+            })
+            ->join('courses', function ($join) use ($id) {
+                $join->on('courses.courseid', '=', 'teacher_course.courseid')
+                    ->where('teacherid', 'LIKE', $id);
+            })
+            ->get();
+        $row_num = $result->count();
+        $result = $this::objectToArray($result);
+        $course = array('row_num' => $row_num, 'row' => $result);
         return view('/teacher/insertHomework')->with('course', $course);
     }
 
@@ -102,18 +113,24 @@ class TeacherController extends Controller {
             session_start();
             $id = $_SESSION['id'];
         }
-        $conn = mysqli_connect("localhost", "root", "ydx970516", "kj");
-        mysqli_select_db($conn, "kj") or die("数据库访问错误" . mysql_error());
-        mysqli_query($conn, "set names UTF8");
-        $sql = "select * from student_homework join course_homework on student_homework.homeworkid = course_homework.homeworkid join teacher_homework on teacher_homework.homeworkid = student_homework.homeworkid join courses on courses.courseid = course_homework.courseid join students on students.id = student_homework.studentid where teacher_homework.teacherid = '" . $id . "'";
-        $result = mysqli_query($conn, $sql);
-        $row_num = mysqli_num_rows($result);
-        $row[0] = NULL;
-        for($i = 0; $i < $row_num; $i++) { 
-            $row[$i] = mysqli_fetch_assoc($result);
-        }
-        $homework = array('row_num' => $row_num, 'row' => $row);
-        mysqli_close($conn);
+        $result = DB::table('student_homework')
+            ->join('course_homework', function ($join) {
+                $join->on('student_homework.homeworkid', '=', 'course_homework.homeworkid');
+            })
+            ->join('teacher_homework', function ($join) {
+                $join->on('teacher_homework.homeworkid', '=', 'student_homework.homeworkid');
+            })
+            ->join('courses', function ($join) {
+                $join->on('courses.courseid', '=', 'course_homework.courseid');
+            })
+            ->join('students', function ($join) use ($id) {
+                $join->on('students.id', '=', 'student_homework.studentid')
+                    ->where('teacher_homework.teacherid', 'LIKE', $id);
+            })
+            ->get();
+        $row_num = $result->count();
+        $result = $this::objectToArray($result);
+        $homework = array('row_num' => $row_num, 'row' => $result);
         return view('/teacher/selectHomework')->with('homework', $homework);
     }
 
@@ -126,12 +143,12 @@ class TeacherController extends Controller {
             session_start();
             $id = $_SESSION['id'];
         }
-        $conn = mysqli_connect("localhost", "root", "ydx970516", "kj");
-        mysqli_select_db($conn, "kj") or die("数据库访问错误" . mysql_error());
-        mysqli_query($conn, "set names UTF8");
-        $result = mysqli_query($conn, "select * from student_homework where homeworkid like '" . $_GET['homeworkid'] . "'");
-        $file = mysqli_fetch_assoc($result);
-        $url = $file['URL'];
+        $homeworkid = $_GET['homeworkid'];
+        $result = DB::table('student_homework')
+            ->where('homeworkid', 'LIKE', $homeworkid)
+            ->get('URL');
+        $result = $this::objectToArray($result);
+        $url = $result[0]['URL'];
         $path = realpath(base_path('storage\app')) . "\\" . $url;
         return response()->download($path);
     }
